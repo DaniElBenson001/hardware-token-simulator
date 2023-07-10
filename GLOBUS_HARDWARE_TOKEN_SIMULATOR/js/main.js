a56e3cef78e950dddf46afa59224068dd5593ff4
DaniElBenson001 <136807060+DaniElBenson001@users.noreply.github.com>
2023-07-10 12:49:00 +0100

    Update main.js

--- GLOBUS_HARDWARE_TOKEN_SIMULATOR/js/main.js
+++ GLOBUS_HARDWARE_TOKEN_SIMULATOR/js/main.js
@@ -1,33 +1,50 @@
-function generateRandomNumbers() {
+//Automatic Timer that counts for 10 secs and loops when it reaches zero
+let seconds = 10;
+function tenSecondsTimer() {
+  generateRandomNumbers();
+  setInterval(function() {
+    if (seconds == 0) {
+      seconds = 10;
+      generateRandomNumbers()
+    }
+    document.getElementById("reset").innerHTML = `Token validity: ${seconds} second(s)`;
+    seconds--;
+  }, 1000);
+}
+tenSecondsTimer();
+
+
+
+//Generate the Random Numbers and Copy them
+  function generateRandomNumbers() {
     var numberList = document.getElementById("numberList");
     numberList.innerHTML = ""; // Clear previous numbers
-
+    
     var min = 0; // Minimum number
     var max = 9; // Maximum number
     var count = 6; // Number of random numbers to generate
-
+    var output = "";
+    
     for (var i = 0; i < count; i++) {
       var randomNumber = Math.floor(Math.random() * (max - min + 1)) + (min);
       var listItem = document.createElement("li");
       listItem.appendChild(document.createTextNode(randomNumber));
       numberList.appendChild(listItem);
+      output += randomNumber;
+    }
+    
+  }
+  copyFunction();
+  function copyFunction(){
+    for(var i = 0; i < count; i++){
+      navigator.clipboard.writeText(output);
     }
   }
+    if(output = randomNumber){
+      console.log("Token Code Copied")
+    }
 
-  var sessionTimeout;
 
-  function resetSessionTimeout() {
-    // Clear existing timeout
-    clearTimeout(sessionTimeout);
-  
-    // Set new timeout
-    sessionTimeout = setTimeout(logout, 60000); // 60 seconds = 60000 milliseconds
-  }
-  
-  function logout() {
-    // Perform logout or session expiration tasks
-    console.log("Session expired. Logging out...");
-  }
-  
-  // Add click event listener to reset the session timeout
-  button.addEventListener("click", resetSessionTimeout);
+//Copyright Year
+// let yearDate = new Date().getFullYear();
+// document.getElementById("year").innerHTML = yearDate;
